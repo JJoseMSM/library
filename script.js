adfb36f0ffbc78c4846dfeffca6f83f940b6bc07
JoseMSM <nueljosema@gmail.com>
2024-05-17 10:54:19 -0600

    Added a preventdefault to the whole form so it doesn't submit a form even if it's empty, working on fixing so it doesn't add the form as multiples

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-function storeInput() {
+const book = document.getElementById("book");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+const submit = document.getElementById("addBook");
+const form = document.getElementById("form1");
+    
     const myLibrary = [];
 
-    const book = document.getElementById("book");
-    const author = document.getElementById("author");
-    const pages = document.getElementById("pages");
-    const read = document.getElementById("read");
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
 
-
-    function Book () {
-        book.value;
-        author.value;
-        pages.value;
-        read.value;
-    }
-
-  
-    myLibrary.push(Book);
-
-    myLibrary.forEach(() => {
-        const card = document.createElement("div");
-        const bookInfo = document.createElement('p');
-            bookInfo.textContent = book.value;
-        const authorInfo = document.createElement('p');
-            authorInfo.textContent = author.value
-        const pageInfo = document.createElement('p');
-            pageInfo.textContent = pages.value;
-
-        card.appendChild(bookInfo); 
-        card.appendChild(authorInfo);
-        card.appendChild(pageInfo);
-
-
-        if(read.checked) {
-            let readInfo = document.createElement('p');
-                readInfo.innerHTML = "Read";
-                card.appendChild(readInfo);
-        } else {
-            let readInfo = document.createElement('p');
-            readInfo.innerHTML = "Not read";
-            card.appendChild(readInfo);
+        function Book () {
+            book.value;
+            author.value;
+            pages.value;
+            read.value;
         }
-        
-        card.classList.add('card');
-        document.getElementById("cardcontainer").appendChild(card);
-    });
-}
+    
+        myLibrary.push(Book);
+
+        myLibrary.forEach(() => {
+    
+            const card = document.createElement("div");
+            const bookInfo = document.createElement('p');
+                bookInfo.textContent = book.value;
+            const authorInfo = document.createElement('p');
+                authorInfo.textContent = author.value
+            const pageInfo = document.createElement('p');
+                pageInfo.textContent = pages.value;
+    
+            card.appendChild(bookInfo); 
+            card.appendChild(authorInfo);
+            card.appendChild(pageInfo);
+    
+            if(read.checked) {
+                let readInfo = document.createElement('p');
+                    readInfo.innerHTML = "Read";
+                    card.appendChild(readInfo);
+            } else {
+                let readInfo = document.createElement('p');
+                readInfo.innerHTML = "Not read";
+                card.appendChild(readInfo);
+            }
+            
+            card.classList.add('card');
+            document.getElementById("cardcontainer").appendChild(card);
+        });
+    })
+
+    
